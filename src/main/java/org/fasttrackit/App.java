8ad91c9c747dc0772dd4cb28f88912de18084220
MirceaPasc <cristy_pasc@yahoo.com>
2018-12-13 21:33:48 +0200

    studied classes, variables and primitive type

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -7,7 +7,25 @@ package org.fasttrackit;
 public class App 
 {
     public static void main( String[] args )
+
     {
-        System.out.println( "Hello World!" );
+        Car car = new Car();
+        car.name = "Dacia";
+        car.color = "white";
+        car.doorCount = 4;
+        car.mileage = 7.5;
+        car.running = true;
+
+        Engine engine = new Engine();
+        engine.manufacturer = "Renault";
+        engine.capacity = 1500;
+
+        car.engine = engine;
+
+        engine = null;
+
+        Car car2 = new Car();
+        car2.name = "sfa";
+
     }
 }
